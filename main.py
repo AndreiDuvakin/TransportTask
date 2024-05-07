2a655afef7698a5eb0a209fb690046bb9fc27540
andrei <andrei@duvakin.ru>
2024-05-07 10:39:03 +0500

    ._.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 import sys
+from pprint import pprint
 
 from PyQt5 import uic
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSpinBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSpinBox, QLabel, QWidget, QSizePolicy
 
 
 class MainWin(QMainWindow):
@@ -12,7 +13,29 @@ class MainWin(QMainWindow):
         uic.loadUi('ui/MainWin.ui', self)
         self.spinBox.valueChanged.connect(self.update_table)
         self.spinBox_3.valueChanged.connect(self.update_table)
+        self.pushButton.clicked.connect(self.lest_math)
         QTimer.singleShot(100, self.update_table)
+
+    def lest_math(self):
+        cost_matrix = []
+        for row in range(self.spinBox_3.value()):
+            row_data = []
+            for column in range(self.spinBox.value()):
+                spin_box = self.tableWidget.cellWidget(row, column)
+                row_data.append(spin_box.value())
+            cost_matrix.append(row_data)
+
+        supply = []
+        for row in range(self.spinBox.value()):
+            spin_box = self.tableWidget.cellWidget(row, self.spinBox.value())
+            if type(spin_box) is QSpinBox:
+                supply.append(spin_box.value())
+
+        demand = []
+        for column in range(self.spinBox_3.value()):
+            spin_box = self.tableWidget.cellWidget(self.spinBox_3.value(), column)
+            if type(spin_box) is QSpinBox:
+                demand.append(spin_box.value())
 
     def update_table(self):
         self.tableWidget.setColumnCount(self.spinBox.value() + 1)
@@ -31,9 +54,13 @@ class MainWin(QMainWindow):
                 if type(self.tableWidget.cellWidget(row, column)) != QSpinBox and not (
                         row == self.tableWidget.rowCount() - 1 and column == self.tableWidget.columnCount() - 1
                 ):
-                    spin_box = QSpinBox(self)
-                    spin_box.setValue(random.randint(0, 30))
+                    spin_box = SpinWidget()
+                    spin_box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+                    spin_box.setValue(random.randint(1, 30))
                     self.tableWidget.setCellWidget(row, column, spin_box)
+                elif row == self.tableWidget.rowCount() - 1 and column == self.tableWidget.columnCount() - 1:
+                    label = QLabel(self)
+                    self.tableWidget.setCellWidget(row, column, label)
 
         self.tableWidget.setHorizontalHeaderLabels(suppliers_headers + ['Запасы'])
         self.tableWidget.setVerticalHeaderLabels(shop_headers + ['Потребности'])
@@ -52,6 +79,18 @@ class MainWin(QMainWindow):
 
         for column in range(self.tableWidget.columnCount()):
             self.tableWidget.setColumnWidth(column, width)
+
+
+class SpinWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/SpinWidget.ui', self)
+
+    def setValue(self, value: int):
+        self.spinBox.setValue(value)
+
+    def value(self):
+        return self.spinBox.value()
 
 
 def except_hook(a, b, c):
