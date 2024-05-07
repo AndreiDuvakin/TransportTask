cd92cf14b94d2ffff2b7ba97c54518b99e2ce833
andrei <andrei@duvakin.ru>
2024-05-07 09:16:48 +0500

    add ui file

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-# Press the green button in the gutter to run the script.
+class MainWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/MainWin.ui', self)
+
+
+def except_hook(a, b, c):
+    sys.__excepthook__(a, b, c)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    win = MainWin()
+    win.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
